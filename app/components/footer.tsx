91e15333039bd4fbee578454a84d61ac9080c8f3
oliplum <oli@globalpodium.com>
2025-02-27 16:16:32 +1300

    version

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className='footer'>
-&copy; {new Date().getFullYear()} Deborah Tsarvousis
+&copy; {new Date().getFullYear()} Deborah Tsarvousis v1.1
         </div>
     );
     }
