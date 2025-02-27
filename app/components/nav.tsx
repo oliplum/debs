8ef23592d3bcf73ff220fdf7a26f53e2a6cafff3
oliplum <oli@globalpodium.com>
2025-02-28 11:10:24 +1300

    images

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -4,13 +4,13 @@ export default function Nav() {
   return (
     <div className='nav'>
 <Link href="/" className='nav-item'>
-  <div >Home</div>
+  <div className="button-text" >Home</div>
 </Link>
 <Link href="http://oligaff.etsy.com" target="_blank" rel="noopener noreferrer" className='nav-item'>
-<div > Shop </div>
+<div className="button-text"> Shop </div>
 </Link>
 <Link href="/contact" className='nav-item'>
-  <div >Contact</div>
+  <div className="button-text">Contact</div>
 </Link>
  
     </div>
